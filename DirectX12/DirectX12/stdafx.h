751aec513239d5b0c107075a567e3113868b234c
matsumoto <matsumoto.k017a1377@gmail.com>
2019-10-07 17:47:41 +0900

    test gpgpu particle

--- DirectX12/DirectX12/stdafx.h
+++ DirectX12/DirectX12/stdafx.h
@@ -1,13 +1,34 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently.
+
 #pragma once
 
-////不要なヘッダーのインクルードを抑制する
 //#ifndef WIN32_LEAN_AND_MEAN
-//#define WIN32_LEAN_AND_MEAN      
+//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
 //#endif
 
-#include "targetver.h"
-
 #define NOMINMAX
-#include <Windows.h>
-#include <wrl/client.h>
-#include "Framework/Utility/Typedef.h"
+#include <windows.h>
+
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <D3Dcompiler.h>
+#include "DX12/d3dx12.h"
+#include <DirectXMath.h>
+//#include <pix3.h>
+
+#include <wrl.h>
+#include <vector>
+#include <shellapi.h>
