e1bf0de0e17bcd404005932d6391d9acdf1de9ff
matsumoto <matsumoto.k017a1377@gmail.com>
2019-10-08 13:36:35 +0900

    move d3dx12.h file

--- DirectX12/DirectX12/Source/stdafx.h
+++ DirectX12/DirectX12/Source/stdafx.h
@@ -25,7 +25,7 @@
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <D3Dcompiler.h>
-#include "DirectX12/Source/DX12/d3dx12.h"
+#include "Framework/Graphics/DX12/d3dx12.h"
 #include <DirectXMath.h>
 
 #include <wrl.h>
